c22d25fd602cb78aaa341c5cba03d72bd7b1cfb4
ReddennOka <ReddennOka.dev@gmail.com>
2025-06-13 15:01:53 +0200

    adding some components files

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -1,4 +1,7 @@
+"use client";
 import Image from "next/image";
+import Button from "@/components/button";
+import { use } from "react";
 
 export default function Home() {
   return (
@@ -12,6 +15,10 @@ export default function Home() {
           height={38}
           priority
         />
+        <Button
+          onClick={() => {
+            alert("Button clicked!");
+          }}>test</Button>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
